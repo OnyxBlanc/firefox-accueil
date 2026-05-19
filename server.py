df7fce94f4dca787b788d380b768099986a0a10e
OnyxBlanc <blancgabrielsn1@gmail.com>
2026-05-19 12:53:59 +0200

    Delete server.py

--- server.py
+++
@@ -1,3 +0,0 @@
-# server.py n'est plus nécessaire.
-# La page fonctionne en ouvrant index.html directement dans Firefox.
-# Pour exporter les événements en CSV, utilise le bouton ↓ CSV dans la page.
